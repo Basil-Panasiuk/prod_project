14f10a84d3f5d2f2ec96f16d8f4b1e128f4151d5
Vasyl Panasiuk <vasyl.panasiuk@tangramltd.com>
2022-09-27 19:35:08 +0300

    1.10 Absolute imports

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
+import { BuildOptions } from './types/config';
 import { ResolveOptions } from 'webpack';
 
-export const buildResolvers = (): ResolveOptions => {
+export const buildResolvers = (options: BuildOptions): ResolveOptions => {
   return {
     extensions: ['.tsx', '.ts', '.js'],
+    modules: [options.paths.src, 'node_modules'],
+    preferAbsolute: true,
+    mainFiles: ['index'],
+    alias: {}, //Homework: add @-alias
   };
 };
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, { Suspense } from 'react'
 import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 export const App = () => {
 	const {theme, toggleTheme} = useTheme();
@@ -16,8 +16,8 @@ export const App = () => {
 		<Link to='/about'>About</Link>
 		<Suspense fallback={<div>Loading chunk...</div>}>
 			<Routes>
-				<Route path='/' element={<MainPageAsync />} />
-				<Route path='/about' element={<AboutPageAsync />} />
+				<Route path='/' element={<MainPage />} />
+				<Route path='/about' element={<AboutPage />} />
 			</Routes>
 		</Suspense>
 	</div>
