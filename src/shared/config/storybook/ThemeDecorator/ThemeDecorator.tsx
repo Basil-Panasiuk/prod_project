76c501325a1ca3fecec00535bc3f86c8df8b0bb6
Basil-Panasiuk <v.panasiuk.job@gmail.com>
2023-04-09 23:23:53 +0300

    fix eslinter

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -3,6 +3,7 @@ import { Theme } from 'app/providers/ThemeProvider';
 
 export const ThemeDecorator =
   (theme: Theme): DecoratorFn =>
+  // eslint-disable-next-line react/display-name
   (StoryComponent) => {
     return (
       <div className={`app ${theme}`}>
