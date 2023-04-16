167ae9a59fcb8ebd1308087ad326ad88427ced03
Basil-Panasiuk <v.panasiuk.job@gmail.com>
2023-04-16 20:57:36 +0300

    Add json-server

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
-// import { useTheme } from 'app/providers/ThemeProvider';
+import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar/ui/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useEffect } from 'react';
 
 export const App: FC = () => {
-  // const { theme } = useTheme();
+  const { toggleTheme } = useTheme();
+
+  useEffect(() => {
+    toggleTheme();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Suspense fallback="i18n loading....">
